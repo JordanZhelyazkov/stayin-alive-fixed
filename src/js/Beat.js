6a9a49414e0a902ae3ff4a2afe34426dd38a36b9
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-21 16:40:48 +0300

    next attemt to fix the problem

--- src/js/Beat.js
+++ src/js/Beat.js
@@ -6,6 +6,32 @@ export default class Beat extends EventEmitter{
       BIT: "bit",
     };
   }
+  
+  
+  constructor(){
+    super();
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    let count = 0;
+    
+    
+
+      setInterval(() => {
+        const message = document.createElement("div");
+        message.classList.add("message");
+        if(count == lyrics.length){
+          count = 0;
+        }
+        message.innerText = lyrics[count];
+        count++
+        document.querySelector(".main").appendChild(message);
+        
+        
+      }, 600);
+    
+    this.emit(Beat.events.BIT);
+
+  }
+  
 
   
 }
